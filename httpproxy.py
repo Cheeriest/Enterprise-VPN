15e34b23a03e7cd2a583abffc32303fffdf83ccb
fima <fimamuzmaher@gmail.com>
2019-10-31 22:07:52 +0200

    Fixed http connections not working.

--- httpproxy.py
+++ httpproxy.py
@@ -1,6 +1,6 @@
 # If we receive a CONNECT request
 
-import socket, threading, ssl
+import socket, threading
 
 MAX_BUFFER = 64 * 512
 class ClientThread(threading.Thread):
@@ -10,68 +10,61 @@ class ClientThread(threading.Thread):
         
 
     def run(self): 
-        
         while True:
             request = self.browser.recv(MAX_BUFFER)
             # parse the first line
-            
-            
+            if 'GET' in request:
+                print request
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             webserver, port = self.get_domain_port(request)
-            if 'CONNECT' in request:
-            # Connect to port 443
-                print request
-                try:
-                    # If successful, send 200 code response
-                    client.connect(( webserver, port ))
-                    reply = "HTTP/1.0 200 Connection established\r\n"
-                    reply += "Proxy-agent: FIMA\r\n"
-                    reply += "\r\n"
-                    self.browser.sendall( reply.encode() )
-                except socket.error as err:
-                    # If the connection could not be established, exit
-                    # Should properly handle the exit with http error code here
-                    break
 
-                
-                """
-                self.ssl_browser = ssl.wrap_socket(self.browser, \
-                                server_side=True, \
-                                certfile='certificate.pem' ,\
-                                keyfile='key.pem',\
-                                do_handshake_on_connect = False)    
-                
-                self.ssl_browser.do_handshake()
-                """
-                self.ssl_browser = self.browser
+            if 'CONNECT' or 'GET' in request:
+            # Connect to port 443
+                if 'CONNECT' in request:
+                    try:
+                        # If successful, send 200 code response
+                        client.connect(( webserver, port ))
+                        reply = "HTTP/1.0 200 Connection established\r\n"
+                        reply += "Proxy-agent: FIMA\r\n"
+                        reply += "\r\n"
+                        self.browser.sendall( reply.encode() )
+                    except socket.error as err:
+                        raise err
+                        # If the connection could not be established, exit
+                        # Should properly handle the exit with http error code here
+                        break
+                else:
+                    client.connect(( webserver, port ))
+                    client.send(request.encode())
+                    self.browser.send(client.recv(MAX_BUFFER))
+
                 # Indiscriminately forward bytes
-                self.ssl_browser.setblocking(0)
+                self.browser.setblocking(0)
                 client.setblocking(0)
                 while True:
                     try:
-                        request = self.ssl_browser.recv(MAX_BUFFER)
+                        request = self.browser.recv(MAX_BUFFER)
                         client.sendall( request )
                         
                     except socket.error as err:
                         pass
                     try:
                         reply = client.recv(MAX_BUFFER)
-                        self.ssl_browser.sendall( reply )
+                        self.browser.sendall( reply )
                     except socket.error as err:
                         pass
 
                 
             
-        print ("Client at ", self.ssl_browser , " disconnected...")
+        print ("Client at ", self.browser , " disconnected...")
 
     def get_domain_port(self, request):
         first_line = request.split('\n')[0]
-
         # get url
         url = first_line.split(' ')[1]
-
+        
         http_pos = url.find("://") # find pos of ://
-        if (http_pos==-1):
+        if (http_pos == -1):
             temp = url
         else:
             temp = url[(http_pos+3):] # get the rest of url
@@ -85,7 +78,7 @@ class ClientThread(threading.Thread):
 
         webserver = ""
         port = -1
-        if (port_pos==-1 or webserver_pos < port_pos): 
+        if (port_pos == -1 or webserver_pos < port_pos): 
 
             # default port 
             port = 80 
@@ -95,20 +88,20 @@ class ClientThread(threading.Thread):
             port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
             webserver = temp[:port_pos] 
 
+        
         return webserver, port
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock = ssl.wrap_socket(sock, server_side=True, certfile='certificate.pem', keyfile='key.pem', do_handshake_on_connect=False)
-    #sock.do_handshake()
+    #Creating the main socket of the proxy.
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('192.168.1.107', 80))
     sock.listen(5)
 
     while True:
         conn, addr = sock.accept()
-        #conn = ssl.wrap_socket(conn, server_side=True, certfile='certificate.pem', keyfile='key.pem')
-        
         newthread = ClientThread(addr, conn)
+        newthread.daemon = True
         newthread.start()
 
     sock.shutdown(socket.SHUT_RDWR)
